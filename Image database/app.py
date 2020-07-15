0aea8cbf0222d0da1b62e188fe25249212bc2587
poornachandra kashi <44542685+poornachandrakashi@users.noreply.github.com>
2020-07-15 22:02:45 +0530

    ugfsjhfdjjk

--- Image database/app.py
+++ Image database/app.py
@@ -37,3 +37,43 @@ def insertBLOB(emp_id, name, photo, biodataFile):
             
             
 insertBLOB(1, "Eric", "aakraman1.jpeg","akraman.txt")
+
+def write_file(data, filename):
+    # Convert binary data to proper format and write it on Hard Disk
+    with open(filename, 'wb') as file:
+        file.write(data)
+
+def readBLOB(emp_id, photo, bioData):
+    print("Reading BLOB data from python_employee table")
+
+    try:
+        connection = mysql.connector.connect(host='localhost',
+                                             database='python_db',
+                                             user='pynative',
+                                             password='pynative@#29')
+
+        cursor = connection.cursor()
+        sql_fetch_blob_query = """SELECT * from python_employee where id = %s"""
+
+        cursor.execute(sql_fetch_blob_query, (emp_id,))
+        record = cursor.fetchall()
+        for row in record:
+            print("Id = ", row[0], )
+            print("Name = ", row[1])
+            image = row[2]
+            file = row[3]
+            print("Storing employee image and bio-data on disk \n")
+            write_file(image, photo)
+            write_file(file, bioData)
+
+    except mysql.connector.Error as error:
+        print("Failed to read BLOB data from MySQL table {}".format(error))
+
+    finally:
+        if (connection.is_connected()):
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+readBLOB(1, "aakraman2.png",
+         "D:\Python\Articles\my_SQL\query_output\eric_bioData.txt")
