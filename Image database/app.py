636c6d4ff78ea0a9d1868ff5b67540e02afc8235
poornachandra kashi <44542685+poornachandrakashi@users.noreply.github.com>
2020-07-15 18:07:00 +0530

    ugfsjhfdjjk

--- Image database/app.py
+++ Image database/app.py
@@ -7,3 +7,33 @@ def convertToBinaryData(filename):
         binaryData = file.read()
     return binaryData
 
+
+def insertBLOB(emp_id, name, photo, biodataFile):
+    print("Inserting BLOB into python_employee table")
+    try:
+        mydb=mysql.connector.connect(host='localhost',user="root",passwd="",database="blogdb")
+
+        cursor = mydb.cursor()
+        sql_insert_blob_query = """ INSERT INTO python_employee
+                          (id, name, photo, biodata) VALUES (%s,%s,%s,%s)"""
+
+        empPicture = convertToBinaryData(photo)
+        file = convertToBinaryData(biodataFile)
+
+        # Convert data into tuple format
+        insert_blob_tuple = (emp_id, name, empPicture, file)
+        result = cursor.execute(sql_insert_blob_query, insert_blob_tuple)
+        mydb.commit()
+        print("Image and file inserted successfully as a BLOB into python_employee table", result)
+
+    except mysql.connector.Error as error:
+        print("Failed inserting BLOB data into MySQL table {}".format(error))
+
+    finally:
+        if (mydb.is_connected()):
+            cursor.close()
+            mydb.close()
+            print("MySQL connection is closed")
+            
+            
+insertBLOB(1, "Eric", "aakraman1.jpeg","akraman.txt")
